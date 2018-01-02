98426b2fd7d5316e008dfacca91e0c9d0ee80266
Chaim - Behalf <chaim@behalf.com>
2018-01-02 13:33:22 +0200

    add sumo logic example for python app engine continaer

--- appengine/python/baseImage/startup.sh
+++ appengine/python/baseImage/startup.sh
@@ -4,11 +4,11 @@ set -x
 HOSTNAME=`curl -s http://169.254.169.254/computeMetadata/v1beta1/instance/hostname`
 
 SUMO_CONF_FILE="/etc/sumo.conf"
-SUMO_CONF_BEHALF_TEMPLATE="/etc/sumo.conf.behalf"
+SUMO_CONF_TEMPLATE="/etc/sumo.conf.company"
 
-if [ -f ${SUMO_CONF_BEHALF_TEMPLATE} ] ; then
-	sed -i 's/ //g' ${SUMO_CONF_BEHALF_TEMPLATE}
-	mv ${SUMO_CONF_BEHALF_TEMPLATE} ${SUMO_CONF_FILE}
+if [ -f ${SUMO_CONF_TEMPLATE} ] ; then
+	sed -i 's/ //g' ${SUMO_CONF_TEMPLATE}
+	mv ${SUMO_CONF_TEMPLATE} ${SUMO_CONF_FILE}
 fi
 
 sed -i "s|@@INSTANCE_ID@@|${HOSTNAME}|g" /opt/SumoCollector/config/sumologic_sources.json
@@ -20,4 +20,4 @@ python --version
 cd app
 export app_folder=$1
 echo app_folder = $app_folder
-gunicorn --access-logfile /opt/behalf/cto/$app_folder/log/${app_folder}_access.log --error-logfile /opt/behalf/cto/$app_folder/log/${app_folder}_error.log --log-file /opt/behalf/cto/$app_folder/log/${app_folder}.log -b :$PORT wsgi
+gunicorn --access-logfile /opt/company/cto/$app_folder/log/${app_folder}_access.log --error-logfile /opt/company/cto/$app_folder/log/${app_folder}_error.log --log-file /opt/company/cto/$app_folder/log/${app_folder}.log -b :$PORT wsgi
